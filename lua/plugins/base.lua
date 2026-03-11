cc5bafb90a9f0e3a0ced9d9bd2d4a6f59a56fa7c
Alex Do <alexxdo2004@gmail.com>
2026-03-11 14:21:43 +0000

    feat: completely refreshed setup for cleannness

---
+++ lua/plugins/base.lua
@@ -0,0 +1,22 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+    },
+  },
+}
